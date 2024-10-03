c3a3c888aa1dc210b1dfcdf41a7d2ebc04ade56b
Seif <saif419713@fci.bu.edu.eg>
2024-10-03 05:05:14 +0300

    Part 05 ASP.NET Core Web APIs - Project Structure

---
+++ LinkDev.Talabat.APIs/Program.cs
@@ -0,0 +1,43 @@
+
+namespace LinkDev.Talabat.APIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var WebApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services
+
+            // Add services to the container.
+
+            WebApplicationBuilder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            WebApplicationBuilder.Services.AddEndpointsApiExplorer();
+            WebApplicationBuilder.Services.AddSwaggerGen(); 
+
+            #endregion
+
+
+
+            var app = WebApplicationBuilder.Build();
+
+            #region Configure Kestrel Middlewares
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers(); 
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}
