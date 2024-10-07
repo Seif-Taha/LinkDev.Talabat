2ee1e9a663ba1e17c0d46d6c94df42e202997cd2
Seif <saif419713@fci.bu.edu.eg>
2024-10-07 20:06:11 +0300

    Part 06 Data Seeding

---
+++ LinkDev.Talabat.Infrastructure.Persistence/Data/StoreContextSeed.cs
@@ -0,0 +1,66 @@
+﻿using LinkDev.Talabat.Core.Domain.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Infrastructure.Persistence.Data
+{
+    public static class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext dbContext) 
+        {
+            if (!dbContext.Brands.Any())
+            {
+
+                var brandsData = await File.ReadAllTextAsync($"../LinkDev.Talabat.Infrastructure.Persistence/Data/Seeds/brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+
+                if (brands?.Count > 0)
+                {
+                    await dbContext.Set<ProductBrand>().AddRangeAsync(brands);
+                    await dbContext.SaveChangesAsync();
+                }
+
+
+            }
+
+            if (!dbContext.Categories.Any())
+            {
+
+                var CategoriesData = await File.ReadAllTextAsync($"../LinkDev.Talabat.Infrastructure.Persistence/Data/Seeds/categories.json");
+                var Categories = JsonSerializer.Deserialize<List<ProductCategory>>(CategoriesData);
+
+
+                if (Categories?.Count > 0)
+                {
+                    await dbContext.Set<ProductCategory>().AddRangeAsync(Categories);
+                    await dbContext.SaveChangesAsync();
+                }
+
+
+            }
+
+            if (!dbContext.Products.Any())
+            {
+
+                var productsData = await File.ReadAllTextAsync($"../LinkDev.Talabat.Infrastructure.Persistence/Data/Seeds/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+
+                if (products?.Count > 0)
+                {
+                    await dbContext.Set<Product>().AddRangeAsync(products);
+                    await dbContext.SaveChangesAsync();
+                }
+
+
+            }
+
+        }
+    }
+}
