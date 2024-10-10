32b860d5e6df6bb60678eb2cfead4e9bd0386a48
Seif <saif419713@fci.bu.edu.eg>
2024-10-10 20:09:37 +0300

    Part 04 Service Manager

--- LinkDev.Talabat.Core.Application/DependancyInjection.cs
+++ LinkDev.Talabat.Core.Application/DependancyInjection.cs
@@ -1,15 +1,7 @@
 ﻿using LinkDev.Talabat.Core.Application.Abstraction;
-using LinkDev.Talabat.Core.Application.Abstraction.Products;
 using LinkDev.Talabat.Core.Application.Mapping;
 using LinkDev.Talabat.Core.Application.Services;
-using LinkDev.Talabat.Core.Application.Services.Products;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Core.Application
 {
@@ -20,8 +12,6 @@ namespace LinkDev.Talabat.Core.Application
         {
             
             services.AddAutoMapper(typeof(MappingProfile));
-
-            services.AddScoped(typeof(IProductService), typeof(ProductService));
 
             services.AddScoped(typeof(IServiceManager), typeof(ServiceManager));
 
