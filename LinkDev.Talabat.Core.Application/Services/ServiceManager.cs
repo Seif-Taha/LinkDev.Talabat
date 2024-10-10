3b3cdbf34e5c8e14dd96db0d9c1808eb63ac7d52
Seif <saif419713@fci.bu.edu.eg>
2024-10-10 22:21:22 +0300

    Part 11 Employee Example - Draft

--- LinkDev.Talabat.Core.Application/Services/ServiceManager.cs
+++ LinkDev.Talabat.Core.Application/Services/ServiceManager.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using LinkDev.Talabat.Core.Application.Abstraction;
+using LinkDev.Talabat.Core.Application.Abstraction.Employees;
 using LinkDev.Talabat.Core.Application.Abstraction.Products;
+using LinkDev.Talabat.Core.Application.Services.Employees;
 using LinkDev.Talabat.Core.Application.Services.Products;
 using LinkDev.Talabat.Core.Domain.Contracts.Persistence;
 using System;
@@ -15,12 +17,15 @@ namespace LinkDev.Talabat.Core.Application.Services
     {
 
         private readonly Lazy<IProductService> _productService;
+        private readonly Lazy<IEmployeeService> _employeeService;
 
         public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _productService = new Lazy<IProductService>(() => new ProductService(unitOfWork, mapper));
+            _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(unitOfWork, mapper));
         }
 
         public IProductService ProductService => _productService.Value;
+        public IEmployeeService EmployeeService => _employeeService.Value;
     }
 }
